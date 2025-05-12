df940d7a69d11ed29ff59687f8fa221be9d4c168
GreyVortex_Dev <prakharv1379@gmail.com>
2025-05-12 21:12:02 +0530

    added basic structure

--- recon.py
+++ recon.py
@@ -9,24 +9,24 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 from colorama import Fore,Style
 from datetime import datetime
-
+#
 #date and time management
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 current_date = now.strftime("%Y-%m-%d")
-
-
+#
+#
 #arguments
 parser = argparse.ArgumentParser(description="Phantom~network~Scanner")
-
+#
 parser.add_argument("-t" , "--target" ,type=str ,required=True, help="target IP or Domain")
 parser.add_argument("-p" , "--port",type=int , help="port number to scan")
-
+#
 #scan range
 parser.add_argument('-pS', action='store_true', help='Common port scan')
 parser.add_argument('-pC', action='store_true', help='Custom port scan')
 parser.add_argument('-pA', action='store_true', help='All ports scan')
-
+#
 #scan types 
 #parser.add_argument('-sP', action='store_true', help='Ping scan')
 parser.add_argument('-sS', action='store_true', help='SYN scan')
@@ -36,10 +36,15 @@ parser.add_argument('-sU', action='store_true', help='UDP scan')
 parser.add_argument('-sF', action='store_true', help='FIN scan')
 parser.add_argument('-sX', action='store_true', help='Xmas scan')
 parser.add_argument('-sN', action='store_true', help='Null scan')
-
-
+#
+#whois lookup flag
+parser.add_argument('-wh', action='store_true', help='whois lookup')
+# Subdomain enumeration and Directory scanner
+parser.add_argument('-sub', action='store_true', help='Subdomain enumeration')
+parser.add_argument('-dir', action='store_true', help='Directory scanner')
+#
 args = parser.parse_args()
-
+#
 #Port Scanner
 common_services = {
     20: "FTP Data",21: "FTP Control",22: "SSH",23: "Telnet",25: "SMTP",53: "DNS",67: "DHCP Server",68: "DHCP Client",
@@ -49,69 +54,129 @@ common_services = {
     1433: "MS SQL Server",1521: "Oracle Database",1723: "PPTP VPN",3306: "MySQL",3389: "RDP",5900: "VNC",
     8080: "HTTP Proxy",8443: "HTTPS Proxy",9200: "Elasticsearch",27017: "MongoDB"
 }
-
+#
 target = args.target
 ports = list(common_services.keys())
+#
+#if you want to modify it , kindly only use functions , no use of classes or any OOP thingy ...
+#OOP will increase my task and slowen the project growth ...
+#And also i am not a fan of OOP ... so please no OOP in this project ...
+#And use comments , so make it more understandable , i might forgot half of comments so its all to you to understand it ...
+#
 
-def common_port_scan(target ,ports):
+###########################################################################################################
+#scan methods
 
-    # TCP scan ...
-    if args.sT:
-        print(Fore.LIGHTWHITE_EX + "[&] TCP  scan...\n"  +Style.RESET_ALL )
-        try:
-            for port in ports:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                sock.settimeout(1)
-                result = sock.connect_ex((target,port))
+# three way handshake (TCP)
 
-                if result == 0:
+def three_way_handshake(target, port):
+        print(Fore.LIGHTBLACK_EX + "[&] TCP  scan...\n"  +Style.RESET_ALL )
+        #scan for common ports
+        if args.pS:           
+            try:
+                for port in ports:
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.settimeout(1)
+                    result = sock.connect_ex((target,port))
+
+                    if result == 0:
                         try:
                             banner = sock.recv(1024).decode(errors="ignore").strip()
-                            print(Fore.LIGHTBLACK_EX + f"[+] {port} ,Banner: {banner}  |OPEN" +Style.RESET_ALL )
+                            print(Fore.LIGHTWHITE_EX + f"[+] {port} Banner: {banner}  |OPEN" +Style.RESET_ALL )
                         except socket.error:
-                            print(Fore.LIGHTBLACK_EX + f"[+] {port} , No Banner |OPEN" +Style.RESET_ALL )
-                        
+                            print(Fore.LIGHTWHITE_EX + f"[+] {port} No Banner |OPEN" +Style.RESET_ALL )       
                 sock.close()
                 time.sleep(0.5)
 
-        except KeyboardInterrupt:
-            print(Fore.RED + "\n[!] Scan interrupted by user."  +Style.RESET_ALL )
-            sys.exit(0)
-        except socket.gaierror:
-            print(Fore.RED +"\n[!] Hostname could not be resolved."  +Style.RESET_ALL )
-            sys.exit(0)
-        except socket.error as e:
-            print(Fore.RED +"\n[!] Couldn't connect to server.",  +Style.RESET_ALL )
-            sys.exit(0)
+            except KeyboardInterrupt:
+                print(Fore.RED + "\n[!] Scan interrupted by user."  +Style.RESET_ALL )
+                sys.exit(0)
+            except socket.gaierror:
+                print(Fore.RED +"\n[!] Hostname could not be resolved."  +Style.RESET_ALL )
+                sys.exit(0)
+            except socket.error as e:
+                print(Fore.RED +"\n[!] Couldn't connect to server.",  +Style.RESET_ALL )
+                sys.exit(0)
+        #scan for all ports
+        if args.pA:
+            pass
+        #scan for custom ports
+        if args.pC:
+            pass
 
-#UDP scan ...
-'''   elif args.sU:
-        print(Fore.BLUE + "[&] UDP scan...\n")
-        try:
-            for port in ports:
-                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                sock.settimeout(1)
-                sock.sendto(b'', (target, port))
-                try:
-                    data, addr = sock.recvfrom(1024)
-                    print(Fore.GREEN + f"[+] {port}  {service} : OPEN (Responded)")
-                except socket.timeout:
-                    print(Fore.RED + f"[-] {port} {service} : OPEN|FILTERED (No response)")
-                finally:
-                    sock.close()
-        except Exception as e:
-            print(Fore.RED + f"[-] {port} : CLOSED|FILTERED")
-        except KeyboardInterrupt:
-            print(Fore.RED + "\n[!] Scan interrupted by user.")
-            sys.exit(0)
-        except socket.gaierror:
-            print(Fore.RED +"\n[!] Hostname could not be resolved.")
-            sys.exit(0)
-        except socket.error:
-            print(Fore.RED +"\n[!] Couldn't connect to server.")
-            sys.exit(0)
-        '''
 
+def syn_scan(target, port):
+    pass
+
+def ack_scan(target, port):
+    pass
+
+def fin_scan(target, port):
+    pass
+
+def xmas_scan(target, port):
+    pass
+
+def null_scan(target, port):
+    pass
+
+def UDP_scan(target, port):
+    #UDP scan ...
+    # elif args.sU:
+    #     print(Fore.BLUE + "[&] UDP scan...\n")
+    #     try:
+    #         for port in ports:
+    #             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    #             sock.settimeout(1)
+    #             sock.sendto(b'', (target, port))
+    #             try:
+    #                 data, addr = sock.recvfrom(1024)
+    #                 print(Fore.GREEN + f"[+] {port}  {service} : OPEN (Responded)")
+    #             except socket.timeout:
+    #                 print(Fore.RED + f"[-] {port} {service} : OPEN|FILTERED (No response)")
+    #             finally:
+    #                 sock.close()
+    #     except Exception as e:
+    #         print(Fore.RED + f"[-] {port} : CLOSED|FILTERED")
+    #     except KeyboardInterrupt:
+    #         print(Fore.RED + "\n[!] Scan interrupted by user.")
+    #         sys.exit(0)
+    #     except socket.gaierror:
+    #         print(Fore.RED +"\n[!] Hostname could not be resolved.")
+    #         sys.exit(0)
+    #     except socket.error:
+    #         print(Fore.RED +"\n[!] Couldn't connect to server.")
+    #         sys.exit(0)
+    pass
+
+
+#########################################################################################################
+#Scan types
+def common_port_scan():
+#scan method chooser
+    if args.sT:
+     three_way_handshake(target, ports)
+    elif args.sS:
+        pass
+    elif args.sU:
+        UDP_scan(target, ports)
+    elif args.sA:
+        pass
+    elif args.sF:
+        pass
+    elif args.sX:
+        pass
+    elif args.sN:
+        pass
+    else:
+        pass
+
+
+#this is a function for whois loopup , flag = -wh
+def whois_lookup(target):
+    pass
+
+#######################################################################################################
 
 if __name__ == "__main__":
     print(Fore.BLUE+ f"Phantom Recon v0.1.1 | By Phantom Group"+Style.RESET_ALL)
@@ -149,8 +214,15 @@ if __name__ == "__main__":
     else:
         print(Fore.LIGHTBLACK_EX + "Invalid Scan Type"  +Style.RESET_ALL )
 
-    print(Fore.LIGHTWHITE_EX+ f"Target: {args.target}  |  Mode: {scan_type}  |  Ports: {scan_range}"+Style.RESET_ALL)
-
+    print(Fore.LIGHTBLACK_EX+ f"Target: {args.target}  |  Mode: {scan_type}  |  Ports: {scan_range}"+Style.RESET_ALL)
+#other functons
+    if args.wh:
+        whois_lookup(target)
+    if args.sub:
+        pass    
+    if args.dir:
+        pass
+#port scanning functions
     if args.pS:
-        common_port_scan(target,ports)
+        common_port_scan()
 
